703803c22648bd3fdfa7942136736ae0a5dedaef
korawitice <69179925+korawitice@users.noreply.github.com>
2020-08-11 03:38:12 +0700

    add routes

---
+++ routes.js
@@ -0,0 +1,32 @@
+'use strict'
+
+// const { route } = require('@adonisjs/framework/src/Route/Manager')
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+//Route.on('/').render('home')
+Route.on('/').render('home')
+Route.on('/home').render('home')
+Route.on('/Contact').render('Contact')
+Route.on('/Shop').render('Shop')
+
+Route.get("/login", "AuthController.login");
+Route.post("/login", "AuthController.loginuser");
+
+Route.get("/register", "AuthController.register");
+Route.post("/register", "AuthController.registeruser");
+
+Route.post("/api/register", "AuthController.registeruser");
+
